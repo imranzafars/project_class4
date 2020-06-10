3d6cae4f381270c46b436292df315dde627d0d14
imranzafars <65555558+imranzafars@users.noreply.github.com>
2020-06-11 01:24:55 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {Message} from './Message.js'
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [isMorning, setMorning] =useState(false)
+// count is variable name
+// setCount will be used to change value of variable count
+// useState(0) will put default value of 0 in count
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}> 
+      <h1>GOOD {isMorning ? 'MORNING' : 'NIGHT'} </h1>
+      <h2>Have {isMorning ? 'a Good Day' : 'some Nice Sleep'} </h2>
+      <Message counter={count}/>
+      <br />
+      <button onClick={
+      //  ()=>alert('Button Pressed')
+        ()=> setCount(++count)
+        }>
+        Increase Counter</button>
+      
+      <button onClick={
+      
+        ()=> setCount(--count)
+        }>
+        Decrease Counter</button>
+
+      <button onClick={
+      
+        ()=> setMorning(!isMorning)
+        }>
+        Change Day / Night</button>
     </div>
   );
 }
 
 export default App;
+// ()=> alert ('Button Pressed') is an anonymous function used.
+// called anonymous becoz no name is given to this function.
+// In Message, counter is the prop in which we are passing count variable used in let function.
+// useState can take any variable type like string, boolean etc.
+// useState transfer value on items under it. It cannot change value in lines above it.
